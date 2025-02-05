5e0faf8257c3b00517ef406d2bcc68aa6597df04
peterglezroa <peterglezroa@gmail.com>
2025-02-05 15:59:13 -0600

    leetcode cpp: Check if one string swap can make string equal

---
+++ leetcode/cpp/1790-check_if_one_string_swap_can_make_string_equal.cpp
@@ -0,0 +1,49 @@
+/* 1790. Check if One String Swap Can Make Strings Equal
+ * You are given two strings s1 and s2 of equal length. A string swap is an
+ * operation where you choose two indices in a string (not necessarily
+ * different) and swap the characters at these indices.
+ * 
+ * Return true if it is possible to make both strings equal by performing at
+ * most one string swap on exactly one of the strings. Otherwise, return false.
+ * 
+ * Example 1:
+ * Input: s1 = "bank", s2 = "kanb"
+ * Output: true
+ * Explanation: For example, swap the first character with the last character of s2 to make "bank".
+ *
+ * Example 2:
+ * Input: s1 = "attack", s2 = "defend"
+ * Output: false
+ * Explanation: It is impossible to make them equal with one string swap.
+ *
+ * Example 3:
+ * Input: s1 = "kelb", s2 = "kelb"
+ * Output: true
+ * Explanation: The two strings are already equal, so no string swap operation is required.
+ * 
+ * Constraints:
+ * 1 <= s1.length, s2.length <= 100
+ * s1.length == s2.length
+ * s1 and s2 consist of only lowercase English letters.
+ */
+
+#include<string>
+
+bool areAlmostEqual(std::string s1, std::string s2) {
+    bool swapped = false;
+    size_t swapPos = -1, length = s1.length(), pos = 0;
+    for ( ; pos < length; pos++) {
+        if (s1[pos] != s2[pos]) {
+            if (swapped) break;
+
+            if (swapPos != -1) {
+                if (s1[pos] != s2[swapPos] || s1[swapPos] != s2[pos]) break;
+                swapped = true;
+            } else {
+                swapPos = pos;
+            }
+        }
+    }
+
+    return ( !(swapPos != -1 && !swapped) && (pos == length) );
+}
